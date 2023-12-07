4a357e477444ea877c1f28e35b60e9b4444c2ebb
Eduardo Vicente <evicente.ti@gmail.com>
2023-12-07 07:49:07 -0500

    post

--- springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -1,10 +1,7 @@
 package net.javaguides.springboot.controller;
 
 import net.javaguides.springboot.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,5 +39,16 @@ public class StudentController {
             @RequestParam String firstName,
             @RequestParam String lastName){
         return new Student(id, firstName, lastName);
+    }
+
+    //Spring boot Rest Api that handles HTTP Post Request
+    //@PostMapping and @RequstBody
+    @PostMapping("student/create")
+    public Student createStudent(@RequestBody Student student){
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+
+        return student;
     }
 }
