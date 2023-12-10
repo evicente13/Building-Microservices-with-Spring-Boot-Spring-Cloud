9bbd163659097ed8e698f82ed5138c085b69882c
Eduardo Vicente <evicente.ti@gmail.com>
2023-12-10 17:59:37 -0500

    get user

---
+++ springboot-restfull-webservices/src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -0,0 +1,32 @@
+package net.javaguides.springboot.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.springboot.entity.User;
+import net.javaguides.springboot.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/users")
+public class UserController {
+
+    private UserService userService;
+
+    //build create User REST API
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User savedUser = userService.createUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    //build get user by id REST API
+    // http://localhost:8080/api/users/1
+    @GetMapping("/{id}")
+    public  ResponseEntity<User> getUserById(@PathVariable("id") Long userId){
+        User user = userService.getUserById(userId);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+}
--- springboot-restfull-webservices/src/main/java/net/javaguides/springboot/service/impl/UserServiceImpl.java
+++ springboot-restfull-webservices/src/main/java/net/javaguides/springboot/service/impl/UserServiceImpl.java
@@ -1,11 +1,34 @@
 package net.javaguides.springboot.service.impl;
 
+import lombok.AllArgsConstructor;
 import net.javaguides.springboot.entity.User;
+import net.javaguides.springboot.repository.UserRepository;
 import net.javaguides.springboot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
     @Override
     public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getUserById(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        return optionalUser.get();
+    }
+
+    @Override
+    public List<User> getAllUsers() {
         return null;
     }
 }
